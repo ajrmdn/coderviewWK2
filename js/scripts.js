cb3b82cee83c32b213f83ad15818dc0c0e1eddeb
Abdellah Ramadan <ajrmdn@gmail.com>
2019-06-07 09:52:24 -0700

    add variables, forms, and a button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
+  $("#survey").submit(function(event) {
+    var userFavAnimal =($("#question1").val());
+    var userFavColor = $("#question2").val();
+    var userFavFood = $("#question3").val();
+    var userFavClams = $("#question4").val();
+    var userCode = $("#question5").val();
+    event.preventDefault();
 
-  });
+
+
+});
